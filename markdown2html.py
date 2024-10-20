2106279cc6959467af927d598775d6f1934ef015
dina <dinahamadi.20@gmail.com>
2024-10-21 00:09:11 +0100

    task 3

--- markdown2html.py
+++ markdown2html.py
@@ -24,22 +24,22 @@ def parse_markdown_to_html(markdown_file, html_file):
                     if in_list:
                         # Close any open list before a heading
                         if is_ordered_list:
-                            html_f.write("</ol>\n")
+                            html_f.write('</ol>\n')
                         else:
-                            html_f.write("</ul>\n")
+                            html_f.write('</ul>\n')
                         in_list = False
                     heading_level = len(line.split()[0])
                     if heading_level <= 6:
                         heading_content = line[heading_level:].strip()
                         html_f.write(
-                            "<h{0}>{1}</h{0}>\n".format(
+                            '<h{0}>{1}</h{0}>\n'.format(
                                 heading_level, heading_content)
                         )
                 elif line.startswith('- '):
                     if not in_list or is_ordered_list:
                         if is_ordered_list:
-                            html_f.write("</ol>\n")
-                        html_f.write("<ul>\n")
+                            html_f.write('</ol>\n')
+                        html_f.write('<ul>\n')
                         in_list = True
                         is_ordered_list = False
                     list_item_content = line[2:].strip()
@@ -47,33 +47,33 @@ def parse_markdown_to_html(markdown_file, html_file):
                 elif line.startswith('* '):
                     if not in_list or not is_ordered_list:
                         if not is_ordered_list:
-                            html_f.write("</ul>\n")
-                        html_f.write("<ol>\n")
+                            html_f.write('</ul>\n')
+                        html_f.write('<ol>\n')
                         in_list = True
                         is_ordered_list = True
                     list_item_content = line[2:].strip()
-                    html_f.write("    <li>{}</li>\n".format(list_item_content))
+                    html_f.write('    <li>{}</li>\n'.format(list_item_content))
                 else:
                     if in_list:
                         if is_ordered_list:
-                            html_f.write("</ol>\n")
+                            html_f.write('</ol>\n')
                         else:
-                            html_f.write("</ul>\n")
+                            html_f.write('</ul>\n')
                         in_list = False
             if in_list:
                 if is_ordered_list:
-                    html_f.write("</ol>\n")
+                    html_f.write('</ol>\n')
                 else:
-                    html_f.write("</ul>\n")
+                    html_f.write('</ul>\n')
 
     except FileNotFoundError:
-        sys.stderr.write("Missing {}\n".format(markdown_file))
+        sys.stderr.write('Missing {}\n'.format(markdown_file))
         sys.exit(1)
 
 
 def main():
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         sys.exit(1)
 
     markdown_file = sys.argv[1]
