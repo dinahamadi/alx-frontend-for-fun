d2a1ac73b2241166c44e3ca1e4e401718a2453d0
dina <dinahamadi.20@gmail.com>
2024-10-20 20:49:27 +0100

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -12,14 +12,14 @@ import os
 
 def main():
 
-    if len(sys.argv) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
     markdown_file = sys.argv[1]
     html_file = sys.argv[2]
 
     if not os.path.isfile(markdown_file):
-        sys.stderr.write("Missing {}\n".format(markdown_file))
+        sys.stderr.write("Missing {}".format(markdown_file))
         sys.exit(1)
 
     sys.exit(0)
