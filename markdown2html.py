b97e2b65e448023f845b599a98af28a9f9f3c236
dina <dinahamadi.20@gmail.com>
2024-10-20 21:01:31 +0100

    0

--- markdown2html.py
+++ markdown2html.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-This  script takes an argument 2 strings:
-First argument is the name of the Markdown file
-Second argument is the output file name
+This script takes 2 arguments:
+First argument is the name of the Markdown file.
+Second argument is the output file name.
 """
-
 
 import sys
 import os
 
 
 def main():
-
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
+
     markdown_file = sys.argv[1]
     html_file = sys.argv[2]
-
-    if not os.path.isfile(markdown_file):
-        sys.stderr.write("Missing {}".format(markdown_file))
+    try:
+        with open(markdown_file, 'r') as f:
+            pass
+    except FileNotFoundError:
+        sys.stderr.write("Missing {}\n".format(markdown_file))
         sys.exit(1)
-
     sys.exit(0)
 
 
