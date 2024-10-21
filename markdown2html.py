9b245a00b544fc637e272f04a6bc69f92d3474ce
dina <dinahamadi.20@gmail.com>
2024-10-21 05:08:24 +0100

    testing1

--- markdown2html.py
+++ markdown2html.py
@@ -4,7 +4,6 @@ This script takes 2 arguments:
 First argument is the name of the Markdown file.
 Second argument is the output file name.
 markdown the first md file into the second html file
-
 """
 
 import sys
