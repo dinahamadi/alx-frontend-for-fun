c0c71c5e820419ba05324ff9cc788eead48b9286
dina <dinahamadi.20@gmail.com>
2024-10-28 01:12:24 +0100

    4

--- markdown2html.py
+++ markdown2html.py
@@ -23,7 +23,8 @@ def parse_markdown_to_html(markdown_file, html_file):
                 heading_level = len(line.split()[0])
                 if heading_level <= 6:
                     heading_content = line[heading_level:].strip()
-                    html_f.write('<h{0}>{1}</h{0}>\n'.format(heading_level, heading_content))
+                    html_f.write('<h{0}>{1}</h{0}>\n'.format(heading_level,
+                                 heading_content))
             elif line.startswith('* ') or line.startswith('- '):
                 if not in_list or (is_ordered_list and line.startswith('- ')):
                     if is_ordered_list:
@@ -33,22 +34,24 @@ def parse_markdown_to_html(markdown_file, html_file):
                     is_ordered_list = False
                 list_item_content = line[2:].strip()
                 html_f.write('    <li>{}</li>\n'.format(list_item_content))
-            elif line.startswith('1. '):  # Ordered list item
-                if not in_list or (not is_ordered_list and line.startswith('- ')):
-                    if not is_ordered_list:
-                        html_f.write('</ul>\n')
-                    html_f.write('<ol>\n')
+            elif line.startswith('* ') or line.startswith('- '):
+                if not in_list or (
+                    not is_ordered_list and line.startswith('- ')
+                ):
+                    if is_ordered_list:
+                        html_f.write('</ol>\n')
+                    html_f.write('<ul>\n')
                     in_list = True
-                    is_ordered_list = True
-                list_item_content = line[3:].strip()
+                    is_ordered_list = False
+                list_item_content = line[2:].strip()
                 html_f.write('    <li>{}</li>\n'.format(list_item_content))
             else:
                 if in_list:
                     html_f.write('</ol>\n' if is_ordered_list else '</ul>\n')
                     in_list = False
                 if line:
-                    # Add <br> for line breaks
-                    html_f.write('<p>{}</p>\n'.format(line.replace('\n', '<br />')))
+                    formatted_line = line.replace('\n', '<br />')
+                    html_f.write('<p>{}</p>\n'.format(formatted_line))
 
         if in_list:
             html_f.write('</ol>\n' if is_ordered_list else '</ul>\n')
